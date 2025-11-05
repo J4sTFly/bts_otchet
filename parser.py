00575b91f1b0805a0da39bedf882b520bd7671a1
J4sTFly <alex.hrynts@gmail.com>
2025-11-05 15:19:13 +0300

    - Implement login logic in parser; - Add scripts for objects population; - Add config.yaml to .gitignore

--- parser.py
+++ parser.py
@@ -1,7 +1,9 @@
 from time import sleep
 from selenium import webdriver
 
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 class Parser(webdriver.Chrome):
@@ -9,18 +11,18 @@ class Parser(webdriver.Chrome):
     server = None
     xpaths = []
 
-    def __init__(self, config=None):
+    def __init__(self, config, start_date, end_date):
         self.config = config
 
         options = Options()
-        #options.add_argument('-headless')
-        options.add_argument('--start-maximized')
-        options.add_argument('--ignore-certificate-errors')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-default-apps')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_experimental_option('excludeSwitches', ["enable-automation"])
-        options.add_experimental_option('useAutomationExtension', False)
+        # options.add_argument('-headless')
+        options.add_argument("--start-maximized")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-default-apps")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option("useAutomationExtension", False)
 
         super().__init__(options=options)
 
@@ -31,11 +33,42 @@ class Parser(webdriver.Chrome):
         servers = config.servers
 
         for server in servers:
-            self.log_in()
+            xpaths = server["xpaths"]
+            self.log_in(server, xpaths)
+            self.wait_for_page_loading()
 
-    def log_in(self):
-        self.get(self.server['url'])
+            self.find_element(By.XPATH, xpaths["archives_section"]).click()
+            self.wait_for_page_loading()
+            # self.populate_objects()
+
+    def log_in(self, server, xpaths):
+        self.get(server["url"])
+
+        self.find_element(By.XPATH, xpaths["username_input"]).send_keys(
+            server["credentials"]
+        )[0]
+        self.find_element(By.XPATH, xpaths["password_input"]).send_keys(
+            server["credentials"]
+        )[1]
+
+        self.find_element(By.XPATH, xpaths["login_btn"]).click()
+
+    def populate_objects(self, server, xpaths):
+        self.execute_script(
+            "Array.from(document.querySelectorAll('.glyphicon-chevron-right')).forEach((el) => {el.click()})"
+        )
+
+    def wait_for_page_loading(self):
+        WebdriverWait(self, 10).until(
+            self.execute_script("return document.readyState") == "complete"
+        )
+
+    def submit_filter_form(self, server, xpaths):
+        dateFrom = self.execute_script(
+            f'$.cookie("RepIndexText", "{server["report_cookie_name"]}")'
+        )
 
 
 if __name__ == "__main__":
-    parser = Parser()
+    parser = Parser(None, None, None)
+    parser.get("https://goolge.com")
